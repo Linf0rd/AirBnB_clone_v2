84880bf81ece1586971254546cc500117fd49dda
Linf0rd <linfordsnagger@gmx.com>
2024-01-09 19:36:52 +0200

    ️ Deploy web_static with Nginx server for smooth serving ️
    Key features:
    - Installs Nginx if not already present, ensuring a web server foundation.
    - Creates a structured directory layout for web_static content, promoting organization.
    - Sets appropriate ownership for data directory for security and access control.
    - Populates an initial test page for validation and quick setup.
    - Establishes a link for seamless current content, enabling easy updates.
    - Configures Nginx to serve web_static content under the /hbnb_static path for clarity.
    - Activates the configuration for immediate use, ensuring a smooth deployment.
    - Restarts Nginx to apply changes and ensure availability of the content.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,24 +2,25 @@
 # This script sets up web servers for the deployment of web_static.
 
 if ! command -v nginx &> /dev/null; then
-  sudo apt update
-  sudo apt install nginx -y
+  sudo apt-get update -y
+  sudo apt-get install nginx -y
 fi
 
-sudo mkdir -p /data/web_static/{releases,shared} /data/web_static/releases/test
-sudo chown -R ubuntu:ubuntu /data
+sudo ufw allow 'Nginx HTTP'
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo chown -hR ubuntu:ubuntu /data
 
 sudo sh -c "echo '<html><body>Holberton School</body></html>' > /data/web_static/releases/test/index.html"
 
 sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 sudo sh -c 'echo "server {
   location /hbnb_static {
     alias /data/web_static/current/;
     index index.html;
   }
-}" > /etc/nginx/sites-available/hbnb_static.conf'
-sudo ln -s /etc/nginx/sites-available/hbnb_static.conf /etc/nginx/sites-enabled/
+}" > /etc/nginx/sites-available/default'
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
 
-sudo systemctl restart nginx
+sudo service nginx restart
